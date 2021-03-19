d7a9fc18d9a90d00d70d059136bfbcf59899c2e0
lanocci <16540010+lanocci@users.noreply.github.com>
2021-03-19 09:58:25 +0900

    add saved suggestion screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,40 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided)
+          );
+        }
+      )
+    );
+  }
+
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -44,6 +75,7 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
   Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
@@ -59,13 +91,18 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
+
 }
